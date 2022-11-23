f6fcfb0042a6ef1c10dcb18388d9931eb241cf84
dvcolin <colin@colindevries.dev>
2022-11-23 14:50:27 -0500

    chore(config): set up eslint, prettier, typescript, commitlint, and husky configs

---
+++ commitlint.config.ts
@@ -0,0 +1,15 @@
+import type { UserConfig } from '@commitlint/types';
+
+const Configuration: UserConfig = {
+  extends: ['@commitlint/config-conventional'],
+  rules: {
+    'scope-enum': [
+      2,
+      'always',
+      ['pages', 'components', 'utils', 'assets', 'api', 'config']
+    ],
+    'scope-empty': [2, 'never']
+  }
+};
+
+module.exports = Configuration;
